8957ef9de2748e8c9b5d0dbed4efcce40e34f3c0
Adrien <adrien.boustie@gmail.com>
2025-06-17 15:06:53 +0200

    ajout route predict

--- app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-
-app = FastAPI()
-
-@app.get("/health")
-def health_check():
-    return JSONResponse(content={"status": "ok"}, status_code=200)
--- main.py
+++ main.py
@@ -6,6 +6,18 @@ import os
 from datetime import datetime
 from typing import Dict, Any
 
+import numpy as np
+
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from sqlalchemy import func
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+import joblib
+import os
+import mlflow
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -114,6 +126,68 @@ async def get_status() -> Dict[str, Any]:
         "environment": os.getenv("ENVIRONMENT", "development"),
         "timestamp": datetime.now().isoformat()
     }
+
+
+
+
+
+MODEL_PATH = os.getenv("MODEL_PATH", "/models/model.pkl")
+DB_PATH = "sqlite:///"+os.getenv("DB_PATH", "/data/db.sqlite3")
+
+orm_model=""
+engine = create_engine(DB_PATH, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+orm_model.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Utilitaires
+def get_model(path):#TODO
+    #changer pour récupérer le dernier modèle.
+    model = mlflow.pyfunc.load_model(model_uri="models:/my_model/latest")
+    model = joblib.load(path)
+    return model
+
+def get_last_dataset(db):
+    """Get last batch of data inserted"""
+    index_col=-1
+    max_index = db.query(func.max(getattr(orm_model, index_col))).scalar()
+    return db.query(orm_model).filter(getattr(orm_model, index_col) == max_index).all()
+
+
+
+
+# Route prédiction
+@app.get("/predict")
+async def predict(db: Session = Depends(get_db)):
+    """predict output of last model for last batch of data"""
+    try:
+        prediction = model.predict(features)
+        model = get_model(MODEL_PATH)
+        dataset = get_last_dataset(db)
+        features = [[row.feature1, row.feature2] for row in dataset]
+        prediction = model.predict(features)
+        if not (prediction.astype(bool)==prediction).all():
+            logger.error(f"Output du modèle non binaire")
+    except Exception as e:
+        message = str(e)
+        status = "Erreur"
+        send_discord_embed(message, status)
+    return {
+        "prediction": list(prediction)
+    }
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     import uvicorn
